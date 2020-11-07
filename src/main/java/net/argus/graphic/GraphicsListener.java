aee7b8b2ee842e9e58d4573d680518a519361257
Olario <66014216+Olario@users.noreply.github.com>
2020-11-07 12:59:31 +0100

    Initial code

--- src/main/java/net/argus/graphic/GraphicsListener.java
+++ src/main/java/net/argus/graphic/GraphicsListener.java
@@ -2,6 +2,6 @@ package net.argus.graphic;
 
 public interface GraphicsListener {
 	
-	public int fonction(int x);
+	public float fonction(float x);
 
 }
--- src/main/java/net/argus/gui/GraphicsPanel.java
+++ src/main/java/net/argus/gui/GraphicsPanel.java
@@ -1,8 +1,10 @@
 package net.argus.gui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,38 +23,57 @@ public class GraphicsPanel extends Panel {
 	private static final long serialVersionUID = -4924292783008457853L;
 	
 	private List<Vector2> vactors = new ArrayList<Vector2>();
-	private GraphicsListener gl;
+	private GraphicsListener graphicsListener;
 	
+	private int lineSize = 2;
 	private Color color;
 	private int x, y;
 	
 	
-	public GraphicsPanel(GraphicsListener gl) {this.gl = gl;}
+	public GraphicsPanel(GraphicsListener graphicsListener) {this.graphicsListener = graphicsListener;}
 	
 	public GraphicsPanel() {}
 	
-	public void addPoint(Vector2 v) {
-		vactors.add(v);
+	/**
+	 * Ajoute un vecteur au graphique
+	 * @param vector
+	 */
+	public void addPoint(Vector2 vector) {
+		vactors.add(vector);
 		getParent().repaint();
 	}
 	
+	/****GETTERS****/
+	public GraphicsListener getGraphicsListener() {return graphicsListener;}
+	public Color getColor() {return color;}
+	public int getLineSize() {return lineSize;}
+	
+	/****SETTERS****/
 	public void setColor(Color color) {this.color = color;}
+	public void setLineSize(int lineSize) {this.lineSize = lineSize;}
+	public void addGraphicsListener(GraphicsListener graphicsListener) {this.graphicsListener = graphicsListener;}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(color!=null?color:g2.getColor());
 		
 		x = getParent().getWidth() / 2;
 		y = getParent().getHeight() / 2;
 		
-		if(gl != null) 
-			for(int i = -10; i < 10; i += 1)
-				g2.drawLine(x + i, y + gl.fonction(i), x + i + 1, y + gl.fonction(i + 1));
+		g2.fillRect(0, y, getParent().getWidth(), 2);
+		g2.fillRect(x, 0, 2, getParent().getHeight());
+		
+		g2.setColor(color!=null?color:g2.getColor());
+		
+		g2.setStroke(new BasicStroke(lineSize));
+		 
+		if(graphicsListener != null) 
+			for(float i = -10000; i < 10000; i += 1f)
+				g2.draw(new Line2D.Float(x + i, y + graphicsListener.fonction(i), x + i + 1, y + graphicsListener.fonction(i + 1)));
 		
 		else
 			for(int i = 0; i < vactors.size() - 1; i++)
-				g2.drawLine((int)vactors.get(i).getX(), (int)vactors.get(i).getY(), (int)vactors.get(i + 1).getX(), (int)vactors.get(i + 1).getY());	
+				g2.draw(new Line2D.Float(vactors.get(i).getX(), vactors.get(i).getY(), vactors.get(i + 1).getX(), vactors.get(i + 1).getY()));
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
@@ -64,11 +85,11 @@ public class GraphicsPanel extends Panel {
 		GraphicsPanel gp = new GraphicsPanel(new GraphicsListener() {
 			
 			@Override
-			public int fonction(int x) {
-				return (x*2) * x^2;
+			public float fonction(float x) {
+				return x * x;
 			}
 		});
-		
+		gp.setColor(Color.CYAN);
 		fen.add(gp);
 		
 		fen.setVisible(true);
