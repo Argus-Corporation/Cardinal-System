49329b442fef85594665f8e0c56d78e182c0d71b
Django-Argus <malaska@mailo.com>
2022-04-23 10:28:39 +0200

    Update

--- src/main/java/net/argus/image/cr/CR.java
+++ src/main/java/net/argus/image/cr/CR.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 public class CR {
 
 	public static void main(String[] args) throws IOException {
-		CRImage cr = new CRImage(1, new File("C:\\Users\\Django\\Desktop\\test.png"));
-		//cr.write("@");
+		CRImage cr = new CRImage(1, new File("D:\\Django\\Document 1\\Git\\Eclipse\\test.png"));
+		//cr.write("Test");
 		System.out.println(cr.read());
 		
 		//System.out.println(new Binary("1010").toInt());
